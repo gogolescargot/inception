c024ab41aa631f9f9b4d0dd5f6392f0bc8b471c3
Gauthier <gauthiergalon@pm.me>
2024-05-28 02:07:51 +0200

    WIP

---
+++ requirements/mariadb/conf/setup.sh
@@ -0,0 +1,4 @@
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS '$DB_NAME';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -e "FLUSH PRIVILEGES;"
